6d08997ce3ff950e237e547bb1e903e050ee2232
vaughn <jwst0208@gmail.com>
2023-11-03 21:04:41 +0900

    add : agent.py

--- method/MC.py
+++ method/MC.py
@@ -1,22 +1,7 @@
 import random
 import numpy as np
 from environment import GridWorld
-
-class Agent():
-    def __init__(self):
-        pass        
-
-    def select_action(self):
-        coin = random.random()
-        if coin < 0.25:
-            action = 0
-        elif coin < 0.5:
-            action = 1
-        elif coin < 0.75:
-            action = 2
-        else:
-            action = 3
-        return action
+from agent import Agent
 
 
 def train_mc(eps):
